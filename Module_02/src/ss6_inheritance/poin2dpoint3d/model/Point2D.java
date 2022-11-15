e2680e78bdd9fe928bdffa3dfb60f7c93342f3c1
tnq112 <79589849+tnq112@users.noreply.github.com>
2022-11-15 07:01:12 +0700

    fixedcomment

--- Module_02/src/ss6_inheritance/poin2dpoint3d/model/Point2D.java
+++ Module_02/src/ss6_inheritance/poin2dpoint3d/model/Point2D.java
@@ -1,8 +1,13 @@
 package ss6_inheritance.poin2dpoint3d.model;
 
+import java.util.Arrays;
 public class Point2D {
-    private float x;
-    private float y;
+    private float x=0.0f;
+    private float y=0.0f;
+
+    public Point2D(){
+
+    }
 
     public Point2D(float x, float y) {
         this.x = x;
@@ -26,21 +31,20 @@ public class Point2D {
         this.y = y;
     }
 
+    public float[] getXY() {
+        return new float[]{this.x, this.y};
+    }
+
     public void setXY(float x, float y) {
         this.y = y;
         this.x = x;
     }
-
-    public float getXY() {
-        float[] arr = new float[]{x, y};
-        return arr[2];
-    }
-
     @Override
     public String toString() {
         return "Point2D{" +
                 "X= " + x +
                 "  Y= " + y +
+                "  Array = " + Arrays.toString(getXY()) +
                 '}';
     }
 }
--- Module_02/src/ss6_inheritance/poin2dpoint3d/model/Point3D.java
+++ Module_02/src/ss6_inheritance/poin2dpoint3d/model/Point3D.java
@@ -1,8 +1,12 @@
 package ss6_inheritance.poin2dpoint3d.model;
 
+import java.util.Arrays;
 public class Point3D extends Point2D {
-    private float z;
+    private float z=0.0f;
 
+    public Point3D(){
+
+    }
     public Point3D(float x, float y, float z) {
         super(x, y);
         this.z = z;
@@ -16,22 +20,22 @@ public class Point3D extends Point2D {
         this.z = z;
     }
 
-    public void setXYZ(float x, float y, float z) {
-//        setX(x);
-//        setY(y);
-        this.z = z;
+    public float[] getXYZ() {
+        return new float[]{super.getX(), super.getY(), this.z};
     }
 
-    public float getXY() {
-        float[] arr = new float[]{getX(), getY(), z};
-        return arr[3];
+    public void setXYZ(float x, float y, float z){
+        super.setX(x);
+        super.setY(y);
+        this.z = z;
     }
 
     @Override
     public String toString() {
-        return "Point3D{" +
-                "X= " + getX() +
-                "  Y= " + getY() +
-                "  z= " + z + '}';
+        return "Point3D{"
+                + "Z= " + z
+                + " Array = "
+                + Arrays.toString(getXYZ())
+                +'}';
     }
 }
