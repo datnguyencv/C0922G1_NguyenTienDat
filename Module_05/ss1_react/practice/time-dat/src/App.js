fcf11ebae009be7a4781e9de13dbc1d98796c708
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-06-05 13:53:29 +0700

    update_ss1_react

--- Module_05/ss1_react/practice/time-dat/src/App.js
+++ Module_05/ss1_react/practice/time-dat/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hello C1222G1
+    </p>
+    <h2>
+      Bây giờ là {new Date().toLocaleTimeString()} ngày {new Date().toLocaleDateString()}.
+    </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
