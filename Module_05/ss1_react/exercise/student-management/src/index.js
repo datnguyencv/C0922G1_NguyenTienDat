41e01709ca5d53df1a6df48ae2224360802c80e9
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-06-05 14:43:06 +0700

    update_ss1-finish

--- Module_05/ss1_react/exercise/student-management/src/index.js
+++ Module_05/ss1_react/exercise/student-management/src/index.js
@@ -6,31 +6,37 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const students = [
   {
+    id:1,
     company: 'Alfreds Futterkiste',
     contact: 'Maria Anders',
     country: 'Germany'
   },
   {
+    id:2,
     company: 'Centro comercial Moctezuma',
     contact: 'Francisco Chang',
     country: 'Mexico'
   },
   {
+    id:3,
     company: 'Ernst Handel',
     contact: 'Roland Mendel',
     country: 'Austria'
   },
   {
+    id:4,
     company: 'Island Trading',
     contact: 'Helen Bennett',
     country: 'UK'
   },
   {
+    id:5,
     company: 'Laughing Bacchus Winecellars',
     contact: 'Yoshi Tannamuri',
     country: 'Canada'
   },
   {
+    id:6,
     company: 'Magazzini Alimentari Riuniti',
     contact: 'Giovanni Rovelli',
     country: 'Italy'
@@ -39,13 +45,18 @@ const students = [
 
 root.render(
   <>
+  <table>
+  <tbody>
     {students.map(student => (
-          <tr>
+          <tr key={student.id}>
             <td>{student.company}</td>
             <td>{student.contact}</td>
             <td>{student.country}</td>
           </tr>
+          
         ))}
+        </tbody>
+        </table>
   </>
 );
 
