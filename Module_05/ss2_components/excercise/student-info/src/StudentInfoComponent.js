752e2e6a374c09d08332994c61c82240910b2289
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-06-06 15:15:33 +0700

    update_ss2

---
+++ Module_05/ss2_components/excercise/student-info/src/StudentInfoComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export class StudentInfoComponent extends React.Component {
+  render() {
+    // Dữ liệu danh sách sinh viên
+    const students = [
+      { id: 1, name: "Tiến Đạt", age: 20, address: "Đà Nẵng" },
+      { id: 2, name: "Minh Thông", age: 22, address: "Quảng Nam" },
+      { id: 3, name: "Văn Trí", age: 21, address: "Quảng Trị" },
+    ];
+
+    return (
+      <div id="student-list">
+        <div className="table-responsive">
+          <table className="table table-primary">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((student) => (
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.age}</td>
+                  <td>{student.address}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ Module_05/ss2_components/excercise/worker-todo-app/src/TodoApp.js
@@ -0,0 +1,44 @@
+import { Component } from "react";
+
+export class TodoApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      item: "",
+      list: ['Làm bài tập','Học bài mới'],
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      item: value,
+    });
+  };
+
+  handleAddItem = () => {
+    this.setState({
+      item: "",
+      list: [this.state.item,...this.state.list],
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div style={{ textAlign: "center" }}>
+            <h2> To do list </h2>
+          <input
+            value={this.state.item}
+            onChange={(event) => this.handleChange(event.target.value)}
+          />
+          <button onClick={() => this.handleAddItem()}> Add</button>
+          <ul>
+            {this.state.list.map((item, index) => (
+              <li style={{listStyle : "none"}} key={index}> * {item}</li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
