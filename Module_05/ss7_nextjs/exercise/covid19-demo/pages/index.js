e48dbf53d618d3a2c3bacd24e0ce82c68aa076b8
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-06-13 15:46:26 +0700

    update_ss8_nextjs

---
+++ Module_05/ss7_nextjs/exercise/covid19-demo/pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import axios from 'axios';
+
+
+export default function Home({covidData}) {
+  return (
+    <main className={styles.main}>
+      <h1>Vietnam COVID-19 Information</h1>
+      <table className={styles.table}>
+        <thead>
+          <tr className={styles.tr}>
+            <th className={styles.th}>ID</th>
+            <th className={styles.th}>Name</th>
+            <th className={styles.th}>Death</th>
+            <th className={styles.th}>Treating</th>
+            <th className={styles.th}>Cases</th>
+            <th className={styles.th}>Recovered</th>
+            <th className={styles.th}>Case Today</th>
+          </tr>
+        </thead>
+        <tbody>
+          {covidData.map((values, index) => (
+            <tr className={styles.tr} key={index}>
+              <td className={styles.td}>{index + 1}</td>
+              <td className={styles.td}>{values.name}</td>
+              <td className={styles.td}>{values.death}</td>
+              <td className={styles.td}>{values.treating}</td>
+              <td className={styles.td}>{values.cases}</td>
+              <td className={styles.td}>{values.recovered}</td>
+              <td className={styles.td}>{values.casesToday}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  )
+}
+
+export async function getServerSideProps() {
+  const response = await axios.get("http://localhost:8080/locations");
+  return {
+    props: {
+      covidData: response.data,
+    },
+  };
+}
